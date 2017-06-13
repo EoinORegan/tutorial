bf601218472b72a58d6f0d018b152bcb979d3577
Eoin ORegan <eoin.oregan@cit.ie>
2017-06-13 14:01:29 +0100

    made speculative changes

--- src/power.py
+++ src/power.py
@@ -1,17 +1,16 @@
 # This is a script that claculates one number to 
 # the power of another.
 def to_the_power(x, y = 2):
-    result = x
+	### trying this out
+	###
+    return x ** y 
+	result = x
     for i in range(0, y):
         result = result * x
     return result
 
-x = 9
-<<<<<<< HEAD
+x = 5 #
 y = 8 # 
-=======
-y = 8
->>>>>>> 3642a5831cd2152a5384496fb8adbff8aad109c6
 print("{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y)))
 print ("The End")
 
